d2154208f48b4b47630511c695f778abfc074b6a
GazizivaElina <mapleshadesf@gmail.com>
2016-11-07 15:53:47 +0300

    test

---
+++ Horses/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.Events;
+
+
+public class Menu : MonoBehaviour {
+
+	public void ChangeScene(string s){
+		SceneManager.LoadScene (s);
+	}
+}
